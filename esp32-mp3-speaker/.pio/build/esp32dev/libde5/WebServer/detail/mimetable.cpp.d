0080610f800d2ff1ed373ca0b30d3659283f1ab3
Anay Goyal <anay.goyal@gmail.com>
2026-07-28 15:02:31 -0700

    Remove .pio build artifacts, add .gitignore, and update READMEs

--- esp32-mp3-speaker/.pio/build/esp32dev/libde5/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/libde5/WebServer/detail/mimetable.cpp.o: \
- /Users/anaygoyal/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- /Users/anaygoyal/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- /Users/anaygoyal/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
